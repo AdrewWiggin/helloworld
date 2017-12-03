ba03069cf9e36af18c98fc74a39c5171f11c17ee
AdrewWiggin <34212526+AdrewWiggin@users.noreply.github.com>
2017-12-03 09:28:48 -0500

    Add files via upload

---
+++ hellow.c
@@ -0,0 +1,8 @@
+/*	Program 1.1 Your Very First C program - Displaying hellow world */
+# include	<stdio.h>
+
+int main (void)
+{
+	printf ( "Hello world!  ");	
+	return 0;
+}
---
+++ tryagain.c
@@ -0,0 +1,10 @@
+/* Program 1.2 Your Second C program */
+#include <stdio.h>
+#include <windows.h>
+
+int main(void)
+{
+	printf("\" If you don't succeed, try, try, try again!\"");
+	system("pause");
+	return 0;
+}
